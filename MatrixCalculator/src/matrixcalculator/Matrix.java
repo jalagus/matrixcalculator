d9315f7f5e2d33e6791cbbaf1a160b0662de8f30
Jarkko Lagus <jalagus@gmail.com>
2012-12-19 20:05:02 +0200

    Laajemmat testit

--- MatrixCalculator/src/matrixcalculator/Matrix.java
+++ MatrixCalculator/src/matrixcalculator/Matrix.java
@@ -3,18 +3,17 @@ package matrixcalculator;
 import java.util.Arrays;
 
 public class Matrix {
-    
+
     /**
      * Matriisin korkeus
      */
     final public int m;
-    
     /**
      * Matriisin leveys
      */
     final public int n;
     final private double[][] values;
-    
+
     /**
      * Muodostaa matriisin annetun kaksiulotteisen double-taulukon pohjalta
      *
@@ -25,7 +24,7 @@ public class Matrix {
         this.n = matrix[0].length;
         this.values = matrix;
     }
-    
+
     /**
      * Laske kaksi matriisia yhteen
      *
@@ -36,19 +35,19 @@ public class Matrix {
         if (!checkDimensions(this, mat)) {
             return null;
         }
-        
+
         double[][] retMatrix = new double[m][n];
         double[][] matrix = mat.getValues();
-        
+
         for (int i = 0; i < m; i++) {
             for (int a = 0; a < n; a++) {
                 retMatrix[i][a] = values[i][a] + matrix[i][a];
             }
-        }          
-        
+        }
+
         return new Matrix(retMatrix);
     }
-    
+
     /**
      * Laske kahden matriisin kertolasku
      *
@@ -59,178 +58,175 @@ public class Matrix {
         if (this.n != mat.m) {
             return null;
         }
-        
+
         double[][] retMatrix = new double[this.m][mat.n];
         double[][] matrix = mat.getValues();
-        
+
         for (int i = 0; i < retMatrix.length; i++) {
             for (int a = 0; a < retMatrix[0].length; a++) {
                 retMatrix[i][a] = matrixPosSum(a, i, values, matrix);
             }
-        }         
-        
+        }
+
         return new Matrix(retMatrix);
     }
-  
+
     private double matrixPosSum(int col, int row, double[][] mat1, double[][] mat2) {
         double sum = 0;
-        
+
         for (int i = 0; i < mat2.length; i++) {
             sum += mat1[row][i] * mat2[i][col];
         }
-        
+
         return sum;
 
     }
-    
+
     /**
      * Kertoo matriisia kertoimella
      *
      * @param coefficent kerroin, jolla matriisia kerrotaan
-     * @return
+     * @return Matrix-olio, jossa kertolaskun tulos
      */
     public Matrix multiply(double coefficent) {
         double[][] matrix = new double[m][n];
-        
+
         for (int i = 0; i < m; i++) {
             for (int a = 0; a < n; a++) {
                 matrix[i][a] = values[i][a] * coefficent;
             }
-        }        
-        
+        }
+
         return new Matrix(matrix);
     }
-    
+
     /**
      * Laskee matriisin determinantin
      *
-     * @return 
+     * @return Matriisin determinantti double-tyyppisenä
      * @throws Exception
      */
     public double determinant() {
         if (this.m != this.n) {
             throw new IllegalArgumentException();
         }
-        
+
         return detHelper(this.values);
     }
-    
-    public double detHelper(double mat[][]) {
+
+    private double detHelper(double mat[][]) {
         if (mat.length == 2 && mat[0].length == 2) {
             return det2x2(mat);
         }
-        
+
         double sum = 0;
-        
+
         for (int i = 0; i < mat[0].length; i++) {
             if (i % 2 == 1) {
                 mat[0][i] *= -1;
-                sum += mat[0][i] * detHelper(stripRow(mat, i));  
+                sum += mat[0][i] * detHelper(stripRow(mat, i));
+            } else {
+                sum += mat[0][i] * detHelper(stripRow(mat, i));
             }
-            else {
-                sum += mat[0][i] * detHelper(stripRow(mat, i));  
-            }
-            
+
         }
-        
+
         return sum;
     }
-    
-    public double[][] stripRow(double mat[][], int index) {
+
+    private double[][] stripRow(double mat[][], int index) {
         double[][] retMatrix = new double[mat.length - 1][mat[0].length - 1];
-        
+
         int x = 0;
-        
+
         for (int i = 0; i < mat[0].length; i++) {
             if (i != index) {
                 for (int a = 1; a < mat.length; a++) {
-                    retMatrix[a-1][x] = mat[a][i];
+                    retMatrix[a - 1][x] = mat[a][i];
                 }
                 x++;
             }
         }
-        
+
         return retMatrix;
     }
-    
-    public double det2x2(double[][] matrix) {
+
+    private double det2x2(double[][] matrix) {
         return (matrix[0][0] * matrix[1][1]) - (matrix[1][0] * matrix[0][1]);
     }
-    
+
     /**
      * Laskee matriisin käänteismatriisin
      *
-     * @return
+     * @return Matrix-olio, jossa käänteismatriisi
      */
     public Matrix inverse() {
         try {
             double det = this.determinant();
-            
+
             if (det == 0) {
                 return null;
             }
-            
+
             double[][] matrix = this.multiply((1 / det)).getValues();
-        
+
             for (int i = 0; i < matrix.length; i++) {
                 for (int a = 0; a < matrix[0].length; a++) {
                     if (a % 2 == 0) {
                         if (i % 2 == 1) {
                             matrix[i][a] *= -1;
                         }
-                    }
-                    else {
+                    } else {
                         if (i % 2 == 0) {
                             matrix[i][a] *= -1;
                         }
                     }
                 }
             }
-            
+
             return new Matrix(matrix);
-        }
-        catch (Exception ex) {
+        } catch (Exception ex) {
             return null;
         }
-    }    
-    
+    }
+
     /**
      * Palauttaa matriisin transpoosin
-     * 
-     * @return
+     *
+     * @return Matrix-olio, jossa matriisin transpoosi
      */
     public Matrix transpose() {
         double[][] retM = new double[this.n][this.m];
-        
+
         for (int i = 0; i < this.m; i++) {
             for (int a = 0; a < this.n; a++) {
                 retM[a][i] = this.values[i][a];
             }
         }
-        
+
         return new Matrix(retM);
     }
-    
+
     /**
      * Laskee matriisin redusoidun porrasmuodon
      *
      * @return Matrix-olio, jossa matriisi on redusoidussa porramuodossa
      */
     public Matrix rref() {
-        
+
         double[][] row = this.getValues();
-        
+
         for (int i = 0; i < row.length; i++) {
             double[][] temp = new double[1][row[i].length];
         }
-        
+
         return null;
     }
-    
+
     /**
      * Palauttaa matriisin ominaisarvot
      *
-     * @return 
+     * @return
      */
     public int[] eigenvalues() {
         return new int[2];
@@ -243,8 +239,8 @@ public class Matrix {
      */
     public int[] eigenvectors() {
         return new int[2];
-    }    
-    
+    }
+
     /**
      * Palauttaa matriisin arvot kaksiulotteisena double-taulukkona
      *
@@ -253,15 +249,15 @@ public class Matrix {
     public double[][] getValues() {
         return values;
     }
-    
+
     private boolean checkDimensions(Matrix m1, Matrix m2) {
         if (m1.m != m2.m || m1.n != m2.n) {
             return false;
         }
-        
+
         return true;
     }
-    
+
     /**
      * Palauttaa matriisin tulostusmuodossa
      *
@@ -270,22 +266,21 @@ public class Matrix {
     @Override
     public String toString() {
         String ret = "";
-        
+
         int padding = 6;
-        
+
         for (int i = 0; i < m; i++) {
             ret += "| ";
             for (int a = 0; a < n; a++) {
                 if (values[i][a] < 0) {
                     ret += Utils.padRight(values[i][a] + "", padding);
-                }
-                else {
+                } else {
                     ret += Utils.padRight(" " + values[i][a], padding);
                 }
             }
             ret += "|\n";
         }
-        
+
         return ret;
     }
 
@@ -305,12 +300,10 @@ public class Matrix {
             return false;
         }
         final Matrix other = (Matrix) obj;
-        
+
         if (!Arrays.deepEquals(this.values, other.values)) {
             return false;
         }
         return true;
     }
-    
-    
 }
---
+++ MatrixCalculator/test/MatrixClassTest.java
@@ -0,0 +1,212 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import matrixcalculator.Matrix;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jalagus
+ */
+public class MatrixClassTest {
+    
+    public MatrixClassTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void konstruktoriAlustaaOikein() {
+        double[][] matA = {{1, 2}, {2, 4}, {3, 4}};
+
+        Matrix A = new Matrix(matA);
+
+        assertEquals(3, A.m);
+        assertEquals(2, A.n);
+
+        double[][] testMatrix = A.getValues();
+
+        for (int i = 0; i < testMatrix.length; i++) {
+            for (int a = 0; a < testMatrix[0].length; a++) {
+                assertEquals(testMatrix[i][a], matA[i][a], 2);
+            }
+        }
+    }
+
+    @Test
+    public void kahdenMatriisinYhteenlaskuPalauttaaOikein() {
+        double[][] matA = {{1, 2}, {2, 4}};
+        double[][] matB = {{1, 2}, {2, 4}};
+        double[][] matC = {{2, 4}, {4, 8}};
+
+        Matrix A = new Matrix(matA);
+        Matrix B = new Matrix(matB);
+        Matrix C = new Matrix(matC);
+
+        assertEquals(C, A.add(B));
+    }
+
+    @Test
+    public void useanMatriisinYhteenlaskuPalauttaaOikein() {
+        double[][] matA = {{1, 2}, {2, 4}};
+        double[][] matB = {{2, 1}, {2, 4}};
+        double[][] matC = {{4, 5}, {6, 12}};
+
+        Matrix A = new Matrix(matA);
+        Matrix B = new Matrix(matB);
+        Matrix C = new Matrix(matC);
+
+        assertEquals(C, A.add(B).add(A));
+    }
+
+    @Test
+    public void matriisinKertolaskuKertoimenKanssaPalauttaaOikein() {
+        double[][] matA = {{1, 2}, {2, 4}};
+        double[][] matB = {{3, 6}, {6, 12}};
+
+        Matrix A = new Matrix(matA);
+        Matrix B = new Matrix(matB);
+
+        assertEquals(B, A.multiply(3));
+    }
+
+    @Test
+    public void matriisienKertolaskuPalauttaaOikein() {
+        double[][] matA = {{1, 4}, {1, 2}};
+        double[][] matB = {{1, 2}, {6, 1}};
+        double[][] matC = {{25, 6}, {13, 4}};
+
+        Matrix A = new Matrix(matA);
+        Matrix B = new Matrix(matB);
+        Matrix C = new Matrix(matC);
+
+        assertEquals(C, A.multiply(B));
+    }
+
+    @Test
+    public void matriisin2x2DeterminanttiLasketaanOikein() {
+        double[][] matA = {{1, 4}, {1, 2}};
+
+        Matrix A = new Matrix(matA);
+        try {
+            assertEquals(A.determinant(), -2, 2);
+        } catch (Exception ex) {
+        }
+    }
+    
+    @Test
+    public void matriisin4x4DeterminanttiLasketaanOikein() {
+        double[][] matA = {{2,3,1,4},{4,3,1,6},{6,2,4,3},{6,5,1,7}};
+
+        Matrix A = new Matrix(matA);
+        
+        try {
+            assertEquals(A.determinant(), 64, 2);
+        } catch (Exception ex) {
+        }
+    }    
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void matriisinDeterminanttiAntaaVirheenVaaranKokoisellaTaululla() {
+        double[][] matA = {{1, 4, 5}, {1, 2, 2}};
+        
+        Matrix A = new Matrix(matA);
+        A.determinant();
+    }
+
+    @Test
+    public void matriisinKaanteismatriisiPalauttaaOikein() {
+        double[][] matA = {{2, 2}, {1, 2}};
+        double[][] matB = {{1, -1}, {-0.5, 1}};
+
+        Matrix A = new Matrix(matA);
+        Matrix B = new Matrix(matB);
+
+        assertEquals(B, A.inverse());
+    }
+
+    @Test
+    public void matriisinKaanteismatriisiPalauttaaTyhjanJosDeterminanttiOnNolla() {
+        double[][] matA = {{2,2},{2,2}};
+
+        Matrix A = new Matrix(matA);
+
+        assertEquals(null, A.inverse());
+    }
+    
+    @Test
+    public void matriisinTranspoosiToimiiOikein() {
+        double[][] matA = {{1, 2, 3}, {4, 5, 6}};
+        double[][] matB = {{1, 4}, {2, 5}, {3, 6}};
+
+        Matrix A = new Matrix(matA);
+        Matrix B = new Matrix(matB);
+
+        assertEquals(B, A.transpose());
+
+        assertEquals(A, A.transpose().transpose());
+    }
+
+    @Test
+    public void transpoosinTranspoosiOnAlkuperainen() {
+        double[][] matA = {{1, 2, 3}, {4, 5, 6}};
+
+        Matrix A = new Matrix(matA);
+
+        assertEquals(A, A.transpose().transpose());
+    }
+
+    @Test
+    public void redusoituPorrasmuotoRakentuuOikein() {
+        double[][] matA = {{1, 2, 0}, {0, 2, 1}, {1, 2, 1}};
+        double[][] matB = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+
+        Matrix A = new Matrix(matA);
+        Matrix B = new Matrix(matB);
+
+        assertEquals(B, A.rref());
+    }
+
+    @Test
+    public void palauttaaOikeatOminaisarvot() {
+        double[][] matA = {{1, 2, 0}, {0, 2, 1}, {1, 2, 1}};
+        double[][] matB = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+
+        Matrix A = new Matrix(matA);
+        Matrix B = new Matrix(matB);
+
+        assertEquals(B, A.rref());
+    }
+
+    @Test
+    public void palauttaaOikeatOminaisvektorit() {
+        double[][] matA = {{1, 2, 0}, {0, 2, 1}, {1, 2, 1}};
+        double[][] matB = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+
+        Matrix A = new Matrix(matA);
+        Matrix B = new Matrix(matB);
+
+        assertEquals(B, A.rref());
+    }
+}
